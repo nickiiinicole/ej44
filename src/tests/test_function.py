10365af9d50cbf01293d47724d57367779e3d282
nickiiinicole <nicki.nicoledc04@gmail.com>
2025-11-17 10:35:14 +0100

    pytest sigue sin funcioanr hice mas tests

--- src/tests/test_function.py
+++ src/tests/test_function.py
@@ -2,5 +2,11 @@ from ej44.main import *
 
 def limits():
     lista=[0,0,1,1,-1,-1, 0,-2,1]
-    assert new_list(lista, is_prime) == [False,False,False,False,False,False, False,False,False]
-    assert new_list(lista, double)  == [0,0,2,2,-2,-2,0,-4,2]
+    lista2 = [0, 1, 2, 3, 4, 5, -1, -2]
+
+    assert new_list(lista, is_prime) == [False,False,False,False,False,False, False,False,False], "error is_prime"
+    assert new_list(lista, double)  == [0,0,2,2,-2,-2,0,-4,2], "error doubles"
+     # asipracitco lamnbda
+    
+    expected_triples = [0, 3, 6, 9, 12, 15, -3, -6]
+    assert new_list(lista, lambda x: x*3) == expected_triples, "Error en triple"
