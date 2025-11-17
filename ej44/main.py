b4bc59574d211225c4f248936c9fadfbb6218e12
nickiiinicole <nicki.nicoledc04@gmail.com>
2025-11-17 09:23:44 +0100

    ej44 documentgando

---
+++ ej44/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from ej44!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ src/ej44/main.py
@@ -0,0 +1,11 @@
+def function_a():
+    return
+
+'''
+Asignar una funcion a un elemento de la lista,
+ES DECIR GUARDAR LA FUNCION COMO UN VALOR MAS
+RECORDAR QUE LA LISTA NO EJECUTA A LAS FUNCIONES
+'''
+def new_list(list, function):
+    
+    return list[:]
